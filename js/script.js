3c4b7b6b7672a26f4b042a8f929bccc9e1ed908a
inga <goshke@gmail.com>
2019-07-19 13:43:26 +0200

    improve code quality

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@ function menuToggle() {
 
   var x = document.getElementById('myNavtoggle');
 
-  if (x.className === 'navtoggle'){ // if the menu is not opened
-    x.className += ' responsive';  // it will be class="navtoggle responsive", which will make the menu open
-    $("#menu-item-contact").attr("onclick","menuToggle()"); // "Contact" menu item needs this function separately, otherwise after clicking it, menu wouldn't close
+  if (x.className === 'navtoggle') { // if the menu is not opened
+    x.className += ' responsive'; // it will be class="navtoggle responsive", which will make the menu open
+    $('#menu-item-contact').attr('onclick', 'menuToggle()'); // "Contact" menu item needs this function separately, otherwise after clicking it, menu wouldn't close
   } else {
     x.className = 'navtoggle'; // if the menu is open (class="navtoggle responsive"), then make it hide
-    $("#menu-item-contact").attr("onclick",""); //we have to remove this function from "contact" menu item, because if it's used without hamburger menu, then the wrong styling is applied
+    $('#menu-item-contact').attr('onclick', ''); // we have to remove this function from "contact" menu item, because if it's used without hamburger menu, then the wrong styling is applied
   }
-
 }
