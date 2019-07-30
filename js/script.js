1769f86fedee8cc47d8e89403bc80f49b28ad9bc
inga <goshke@gmail.com>
2019-07-30 21:53:22 +0200

    create half finished hamburger menu

--- js/script.js
+++ js/script.js
@@ -11,4 +11,46 @@ function menuToggle () {
     x.className = 'navtoggle'; // if the menu is open (class="navtoggle responsive"), then make it hide
     $('#menu-item-contact').attr('onclick', ''); // we have to remove this function from "contact" menu item, because if it's used without hamburger menu, then the wrong styling is applied
   }
+
+  // toggles class for animated hamburger icon either to open (X), or default (3 bars)
+  $('.animated-ham-icon').toggleClass('open');
 }
+
+// ****
+//
+// Animates hamburger menu icon and toggles the nav menu
+//
+// ****
+function toggleNav () {
+  var hamIcon = document.getElementById('ham-icon');
+
+  // animate from 3 bars to X or vice versa
+  $('.animated-ham-icon').toggleClass('open');
+
+  // if the hamburger icon has the class open then open the menu as well, otherwise close the menu
+  if (hamIcon.className === 'animated-ham-icon open') {
+    document.getElementById('overlay').style.height = '50%';
+  } else {
+    document.getElementById('overlay').style.height = '0%';
+  }
+}
+
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('header').style.top = "0";
+  } else {
+    document.getElementById('header').style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+// $(document).ready(function () {
+//
+//   $('.first-button').on('click', function () {
+//
+//
+//   });
+//
+// });
